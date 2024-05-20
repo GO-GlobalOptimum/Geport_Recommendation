184b5575cd27ab75f8a9eddfc6ff03cc89753385
taewan2002 <taewan2002@naver.com>
2024-05-20 09:29:09 +0900

    Refactor train endpoint to use background tasks

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 
 app = FastAPI()
 
 @app.get('/train')
-async def train():
+async def get_train(background_tasks: BackgroundTasks):
 	'''
 	Background Task
-	
+
 	1. Data Preprocessing
-	2. Model Training
-	3. Model Evaluation
-	4. Model Saving
-	5. Save Model to Cloud Storage
+	2. Model Training: Alert Slack Channel
+	3. Save Model to Cloud Storage
+	4. Generate Predictions top 500
+	5. Save Predictions to Redis Cache
+	6. Alert Slack Channel: Report Training Status
 
-	retrun: wandb run url
 	'''
+	background_tasks.add_task(train)
+
+	return {"message": "Started Training!"}
+
+async def train():
 	
-	return {"message": "Started Training!", "wandb_url": "https://wandb.ai/username/project/runs/run_id"}
+	pass
 
-@app.get('/predict')
-async def predict(user_id: int):
 
-	return {"message": "Welcome Home!"}
-
-@app.get('/start')
-async def start():
+async def data_preprocessing():
 	'''
-	Start Model REST API Server
-
-	1. Load Model from Cloud Storage
-	2. Start Model REST API Server
-
+	database에서 데이터를 가져와서 학습에 적합한 형태로 변환
 	'''
-	return {"message": "Model REST API Server Started!"}
+	pass
 
-@app.get('/stop')
-async def stop():
+async def model_training():
 	'''
-	Stop Model REST API Server
-
-	1. Stop Model REST API Server(Stop model serving, kill the process)
-
+	전처리 시킨 데이터를 이용하여 모델을 학습
 	'''
-	return {"message": "Model REST API Server Stopped!"}
+	pass
+
+async def save_model_to_cloud_storage():
+	'''
+	학습된 모델을 클라우드 스토리지에 저장
+	'''
+	pass
+
+async def generate_predictions_top_500():
+	'''
+	학습된 모델을 이용하여 모든 유저의 상위 500개의 예측값을 생성
+	'''
+	pass
+
+async def save_predictions_to_redis_cache():
+	'''
+	생성된 예측값을 redis cache에 저장
+	'''
+	pass
+
+async def alert_slack_channel():
+	'''
+	학습이 완료되었음을 slack 채널에 알림
+	학습 보고서 전송
+	'''
+	pass
